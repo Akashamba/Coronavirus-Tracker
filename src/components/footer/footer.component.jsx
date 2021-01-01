b007078ad02739929273d089b895629033c634cc
Akashamba <akashambashankar@gmail.com>
2021-01-01 09:46:06 +0530

    added feedback link

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -25,6 +25,7 @@ const Footer = () => {
                 </Nav>
             </Navbar>
             <Navbar className='footer' color="light"><NavbarText className='m-auto'>Stay Home, Stay Safe</NavbarText></Navbar>
+            <Navbar className='footer' color="light"><NavbarText className='m-auto'>Any comments or suggestions? Let me know <a href="https://akashamba-feedback.netlify.app/coronavirus-tracker">here</a>.</NavbarText></Navbar>
             <Navbar className='footer' color="light"><NavbarText className='m-auto'>&copy; <a href="https://akashamba-portfolio.netlify.app/" target="none" id="portfolio">Akash Ambashankar</a></NavbarText></Navbar>
             <Navbar className='footer' color="light"><NavbarText className='m-auto'><a href="https://github.com/akashamba"><i id="github" className="fab fa-github"></i></a> &emsp; <a href="https://in.linkedin.com/in/akash-ambashankar-48b8111b7"><i id="linkedin" className="fab fa-linkedin"></i></a></NavbarText></Navbar>
         </div>

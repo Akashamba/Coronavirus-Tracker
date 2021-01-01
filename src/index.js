1434ed97e0956d1e00cd6a57c98e300cad0a38fb
Akashamba <akashambashankar@gmail.com>
2021-01-01 17:31:45 +0530

    pwa compliant

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './components/scroll-to-top/scroll.component.jsx';
+import * as serviceWorker from './serviceWorkerRegistration';
 
 ReactDOM.render(
 
@@ -21,3 +22,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
